4b56bfab1b1822bbdec69a0f9d7b6b6df089890d
Megan <megan.lkm@gmail.com>
2015-10-02 16:04:21 -0500

    refactor; add key binding

--- mkfile_backup.py
+++ mkfile_backup.py
@@ -1,21 +1,24 @@
-import os
-import random
-import sublime, sublime_plugin
+from os import path
+from random import getrandbits
 from shutil import copy2
+import sublime
+import sublime_plugin
 
 
 class MkFileBackupCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         win_vars = self.view.window().extract_variables()
-        newpath = self.generate_filename(win_vars)
+        newpath = self.get_new_path(win_vars)
         copy2(win_vars['file'], newpath)
         sublime.status_message('backup file saved: {}'.format(newpath))
 
-    def generate_filename(self, window_vars):
-        newfilepath = os.path.join(window_vars['file_path'], '{}-backup.{}'.format(window_vars['file_base_name'], window_vars['file_extension']))
-        while os.path.exists(newfilepath):
-            newname = '{}-backup-{}.{}'.format(window_vars['file_base_name'],
-                                               str(random.getrandbits(32)),
-                                               window_vars['file_extension'])
-            newfilepath = os.path.join(window_vars['file_path'], newname)
+    def get_new_path(self, window_vars):
+        newfilepath = path.join(window_vars['file_path'], self.generate_filename(window_vars))
+        while path.exists(newfilepath):
+            newfilepath = path.join(window_vars['file_path'], self.generate_filename(window_vars))
         return newfilepath
+
+    def generate_filename(self, window_vars):
+        return '{}-backup-{}.{}'.format(window_vars['file_base_name'],
+                                        str(getrandbits(32)),
+                                        window_vars['file_extension'])
